665194ba6279d2522d8a3b916936ce93a91d8fca
pouya <pouyapouryaie@gmail.com>
2025-06-22 17:05:19 -0300

    first commit

---
+++ src/main/java/ir/bigz/spring/resilient_patterns/ResilientPatternsApplication.java
@@ -0,0 +1,13 @@
+package ir.bigz.spring.resilient_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResilientPatternsApplication.class, args);
+	}
+
+}
